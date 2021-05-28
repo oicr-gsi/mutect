ae7ae4ebdb90494cf539778aef48f376e98f019c
Angie Mosquera <amosquera@oicr.on.ca>
2021-05-28 13:26:52 -0400

    Convert to Vidarr

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,7 +6,7 @@ set -o pipefail
 cd $1
 ls | sed 's/.*\.//' | sort | uniq -c
 echo "Numbers of variants:"
-find . -name "*vcf.gz" -follow -type f -printf "echo -n \"%p \"; zcat %p | grep -v '^#' | wc -l;" | bash
+find . -name "*vcf.gz" -follow -xtype f -printf "echo -n \"%p \"; zcat %p | grep -v '^#' | wc -l;" | bash
 
 module load jq
 jq -c . *.callability_metrics.json
